8ac61e57886604171b4745ad294e07de35804774
Wesley <wesley.contato@hotmail.com>
2020-11-20 07:58:14 -0300

    Projeto inicial / Imutabilidade e Substring

---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?argumentos";
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(7);
+            Console.WriteLine(argumentos);
+
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
