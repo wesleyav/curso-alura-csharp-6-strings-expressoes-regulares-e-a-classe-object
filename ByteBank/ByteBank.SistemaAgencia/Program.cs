ef206c298086280d41da6096c16457c0de32657d
Wesley <wesley.contato@hotmail.com>
2020-11-21 09:16:06 -0300

    Método Equals

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -27,6 +27,30 @@ namespace ByteBank.SistemaAgencia
             Console.WriteLine("Resultado " + contaToString);
             Console.WriteLine(conta);
 
+
+
+            Cliente carlos_1 = new Cliente();
+            carlos_1.Nome = "Carlos";
+            carlos_1.CPF = "458.623.120-03";
+            carlos_1.Profissao = "Designer";
+
+            Cliente carlos_2 = new Cliente();
+            carlos_2.Nome = "Carlos";
+            carlos_2.CPF = "458.623.120-03";
+            carlos_2.Profissao = "Designer";
+
+            ContaCorrente conta_2 = new ContaCorrente(456, 87654);
+
+
+            if (carlos_1.Equals(carlos_2))
+            {
+                Console.WriteLine("São iguais!");
+            }
+            else
+            {
+                Console.WriteLine("Não são iguais!");
+            }
+
             Console.ReadLine();
         }
 
